ea726dad430483594a7e7891e408fc2776992ddc
andrew adeoti <quadrippleA@outlook.com>
2024-04-26 22:48:40 +0100

    back to basics

--- Weather app.cpp
+++ Weather app.cpp
@@ -1,47 +1,57 @@
 #include <iostream>
+#include <fstream>
 #include <curl/curl.h>
 #include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <string>
 
 using namespace rapidjson;
+using namespace std;
 
-// Callback function to write the received data
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output) {
     size_t total_size = size * nmemb;
     output->append(static_cast<char*>(contents), total_size);
     return total_size;
 }
 
+void saveOutputToFile(const string& output) {
+    ofstream outputFile("weather_output.txt");
+    if (outputFile.is_open()) {
+        outputFile << output;
+        cout << "Output saved to 'weather_output.txt'." << endl;
+        outputFile.close();
+    }
+    else {
+        cerr << "Unable to save output to file." << endl;
+    }
+}
+
 int main() {
-    // Initialize libcurl
     CURL* curl = curl_easy_init();
 
     if (curl) {
-        std::string userInput;
-        std::cout << "Enter location: ";
-        std::cin >> userInput;
+        string userInput;
 
-        // Construct URL for geocoding API
-        std::string geocodingUrl = "https://geocoding-api.open-meteo.com/v1/search?name=";
+        cout << "Enter location: ";
+        cin >> userInput;
+
+        string geocodingUrl = "https://geocoding-api.open-meteo.com/v1/search?name=";
         geocodingUrl += userInput + "&count=1&language=en&format=json";
 
         curl_easy_setopt(curl, CURLOPT_URL, geocodingUrl.c_str());
 
-        // Set the callback function to handle received data
-        std::string response_data;
+        string response_data;
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
 
-        // Perform the HTTP GET request
         CURLcode res = curl_easy_perform(curl);
-
-        // Check for errors
         if (res != CURLE_OK) {
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
             curl_easy_cleanup(curl);
-            return 1; // return error code
+            return 1;
         }
 
-        // Parse the JSON string
         Document parsedData;
         parsedData.Parse(response_data.c_str());
 
@@ -49,17 +59,76 @@ int main() {
             double latitude = parsedData["results"][0]["latitude"].GetDouble();
             double longitude = parsedData["results"][0]["longitude"].GetDouble();
 
-            // Output latitude and longitude
-            std::cout << "Latitude: " << latitude << std::endl;
-            std::cout << "Longitude: " << longitude << std::endl;
+            string weatherUrl = "https://api.open-meteo.com/v1/forecast?latitude=";
+            weatherUrl += to_string(latitude) + "&longitude=" + to_string(longitude) + "&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m";
+
+            curl_easy_setopt(curl, CURLOPT_URL, weatherUrl.c_str());
+
+            string weatherData;
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &weatherData);
+
+            res = curl_easy_perform(curl);
+            if (res != CURLE_OK) {
+                fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+                curl_easy_cleanup(curl);
+                return 1;
+            }
+
+            Document weatherParsedData;
+            weatherParsedData.Parse(weatherData.c_str());
+
+            if (weatherParsedData.HasMember("current_weather")) {
+                const Value& currentWeather = weatherParsedData["current_weather"];
+                double temperature = currentWeather["temperature"].GetDouble();
+                double windSpeed = currentWeather["windspeed"].GetDouble();
+                int windDirection = currentWeather["winddirection"].GetInt();
+                int weatherCode = currentWeather["weathercode"].GetInt();
+
+                cout << "Latitude: " << latitude << endl;
+                cout << "Longitude: " << longitude << endl;
+                cout << "Temperature: " << temperature << " °C" << endl;
+                cout << "Wind Speed: " << windSpeed << " m/s" << endl;
+                cout << "Wind Direction: " << windDirection << " degrees" << endl;
+                cout << "Weather Code: " << weatherCode << endl;
+                cout << "Hourly Data:" << endl;
+
+                if (weatherParsedData.HasMember("hourly")) {
+                    const Value& hourly = weatherParsedData["hourly"];
+                    const Value& time = hourly["time"];
+                    const Value& hourlyTemperature = hourly["temperature_2m"];
+                    const Value& hourlyHumidity = hourly["relativehumidity_2m"];
+                    const Value& hourlyWindSpeed = hourly["windspeed_10m"];
+
+                    for (SizeType i = 0; i < time.Size(); ++i) {
+                        cout << "Time: " << time[i].GetString() << endl;
+                        cout << "Temperature: " << hourlyTemperature[i].GetDouble() << " °C" << endl;
+                        cout << "Humidity: " << hourlyHumidity[i].GetDouble() << "%" << endl;
+                        cout << "Wind Speed: " << hourlyWindSpeed[i].GetDouble() << " m/s" << endl;
+                        cout << endl;
+
+                        // Exit the program after printing the first hourly data point
+                        if (i == 0) {
+                            cout << "Do you want to save the output to a file? (Y/N): ";
+                            char choice;
+                            cin >> choice;
+                            if (choice == 'Y' || choice == 'y') {
+                                saveOutputToFile(weatherData);
+                            }
+                            curl_easy_cleanup(curl);
+                            return 0;
+                        }
+                    }
+                }
+            }
+            else {
+                cerr << "Error: Unable to parse weather data response." << endl;
+            }
         }
         else {
-            std::cerr << "Error: Unable to parse geocoding response." << std::endl;
-            curl_easy_cleanup(curl);
-            return 1; // return error code
+            cerr << "Error: Unable to parse geocoding response." << endl;
         }
 
-        // Cleanup
         curl_easy_cleanup(curl);
     }
 
--- menu.cpp
+++ menu.cpp
@@ -67,70 +67,70 @@
 
 
 
-#include <iostream>
-#include "menu.h"
-
-using namespace std;
-
-void mainMenu() {
-    int choice;
-    bool confirmed = false;
-
-    do {
-        cout << "===== Main Menu =====" << endl;
-        cout << "1. Back to Main Menu" << endl;
-        cout << "2. Add Favorite" << endl;
-        cout << "3. Delete Favorites" << endl;
-        cout << "4. View History" << endl;
-        cout << "5. Save" << endl;
-        cout << "6. Delete" << endl;
-        cout << "7. Check Weather Data" << endl;
-        cout << "8. Exit" << endl;
-
-        cout << "Enter your choice (1-8): ";
-        cin >> choice;
-
-        // Prompt for confirmation
-        cout << "Are you sure you want to select option " << choice << "? (Y/N): ";
-        char confirmChoice;
-        cin >> confirmChoice;
-
-        if (confirmChoice == 'Y' || confirmChoice == 'y') {
-            confirmed = true;
-        }
-        else {
-            confirmed = false;
-        }
-    } while (!confirmed);
-
-    // Perform actions based on user's confirmed choice
-    switch (choice) {
-    case 1:
-        // Add code to handle Back to Main Menu option
-        break;
-    case 2:
-        // Add code to handle Add Favorite option
-        break;
-    case 3:
-        // Add code to handle Delete Favorites option
-        break;
-    case 4:
-        // Add code to handle View History option
-        break;
-    case 5:
-        // Add code to handle Save option
-        break;
-    case 6:
-        // Add code to handle Delete option
-        break;
-    case 7:
-        // Add code to handle Check Weather Data option
-        break;
-    case 8:
-        // Add code to handle Exit option
-        break;
-    default:
-        cout << "Invalid choice. Please enter a number between 1 and 8." << endl;
-        break;
-    }
-}
+//#include <iostream>
+//#include "menu.h"
+//
+//using namespace std;
+//
+//void mainMenu() {
+//    int choice;
+//    bool confirmed = false;
+//
+//    do {
+//        cout << "===== Main Menu =====" << endl;
+//        cout << "1. Back to Main Menu" << endl;
+//        cout << "2. Add Favorite" << endl;
+//        cout << "3. Delete Favorites" << endl;
+//        cout << "4. View History" << endl;
+//        cout << "5. Save" << endl;
+//        cout << "6. Delete" << endl;
+//        cout << "7. Check Weather Data" << endl;
+//        cout << "8. Exit" << endl;
+//
+//        cout << "Enter your choice (1-8): ";
+//        cin >> choice;
+//
+//        // Prompt for confirmation
+//        cout << "Are you sure you want to select option " << choice << "? (Y/N): ";
+//        char confirmChoice;
+//        cin >> confirmChoice;
+//
+//        if (confirmChoice == 'Y' || confirmChoice == 'y') {
+//            confirmed = true;
+//        }
+//        else {
+//            confirmed = false;
+//        }
+//    } while (!confirmed);
+//
+//    // Perform actions based on user's confirmed choice
+//    switch (choice) {
+//    case 1:
+//        // Add code to handle Back to Main Menu option
+//        break;
+//    case 2:
+//        // Add code to handle Add Favorite option
+//        break;
+//    case 3:
+//        // Add code to handle Delete Favorites option
+//        break;
+//    case 4:
+//        // Add code to handle View History option
+//        break;
+//    case 5:
+//        // Add code to handle Save option
+//        break;
+//    case 6:
+//        // Add code to handle Delete option
+//        break;
+//    case 7:
+//        // Add code to handle Check Weather Data option
+//        break;
+//    case 8:
+//        // Add code to handle Exit option
+//        break;
+//    default:
+//        cout << "Invalid choice. Please enter a number between 1 and 8." << endl;
+//        break;
+//    }
+//}
